adc02667a26538cb6590d56b4f7ae09782119df9
bipinbachhao <bipinbachhao@gmail.com>
2018-03-25 11:57:18 -0400

    Tested code, currently only prints names of instances and not deleting nodes

--- clean_nodes.sh
+++ clean_nodes.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 for node in $(knife search node "ec2:* AND ohai_time:[* TO $(date +%s -d '3 days ago')]" -i); do
-  nslookup $node
+  nslookup $node > /dev/null 2>&1
   status=$( echo $? )
   if [[ $status == 1 ]] ; then
     ip_address=$(knife node show $node -a ipaddress|grep ipaddress|awk '{print $2}')
